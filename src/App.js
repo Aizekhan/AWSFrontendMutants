ab773b3b6aca2b2f40dce6854de27ce5b4169517
grucia3 <grucia3@gmail.com>
2024-07-15 05:24:27 +0300

    Add .gitignore and .gitattributes for line ending normalization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        fetch('https://your-backend-app-name.herokuapp.com')
+            .then(response => response.text())
+            .then(data => setMessage(data));
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>{message}</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
